d33c4404507a8a4818cd81c90bfeac4da355d918
Arthur Mury <arthur.mury@outlook.com>
2022-07-31 21:43:56 -0300

    Alterações de layout e nova funcionalidade
    1. Funcionamento do filtro para pesquisa de cards;
    2. Ajuste do layout dos cards;
    3. Mudança de cor do background;

--- cards-applitcation/src/App.jsx
+++ cards-applitcation/src/App.jsx
@@ -1,45 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Card, Navbar, Form, Container, InputGroup, Button, Row, Col } from 'react-bootstrap';
-import { AiOutlineSearch } from 'react-icons/ai';
-import CardsList from './components/CardsList'
+import { Navbar } from 'react-bootstrap';
+import CardsList from './components/CardsList';
 
 function App() {
   return (
     <>
-      <Navbar id="header-title">
+      <Navbar id="header-title" fixed="top">
         <Navbar.Brand id="header-logo" />
       </Navbar>
-
-      <Row>
-        <Container fluid id="search-container">
-          <Container>
-            <InputGroup id="search-bar">
-              <Form.Control
-                placeholder="Digite aqui sua busca..."
-                aria-label="Digite aqui sua busca..."
-              >
-              </Form.Control>
-              <Button variant="light" id="button-addon">
-                <AiOutlineSearch />
-              </Button>
-            </InputGroup>
-          </Container>
-        </Container>
-      </Row>
-
-      <Container id="result-container">
-        <Row id="results-title-row">
-          <Col md={6} xs={6}>
-            <h1 id="results-title">Resultado de pesquisa</h1>
-          </Col>
-          <Col md={6} xs={6}>
-            <Button id="btn-new-card">Novo card</Button>
-          </Col>
-        </Row>
-        <Row>
-          <CardsList />
-        </Row>
-      </Container>
+      <CardsList />
     </>
   );
 }
--- cards-applitcation/src/components/CardsList.jsx
+++ cards-applitcation/src/components/CardsList.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import { Card, Button, Row, Col } from 'react-bootstrap';
+import { Card, Image, Form, Container, InputGroup, Button, Row, Col } from 'react-bootstrap';
+import { AiOutlineSearch } from 'react-icons/ai';
 import axios from "axios"
 
 const url = "https://pokeapi.co/api/v2/pokemon";
@@ -10,7 +11,8 @@ class CardsList extends Component {
 
         this.state = {
             Items: [],
-            AllPokeData: []
+            AllPokeData: [],
+            SearchValue: null
         }
     }
 
@@ -30,7 +32,6 @@ class CardsList extends Component {
     //Acessa um dado pokemon, e retorna um objeto contendo nome e url de seu sprite
     GetDataImage(previousResponse) {
         let items = previousResponse;
-        // let items = this.state.Items;
         let allPokeData = [];
 
         items.map(async element => {
@@ -47,10 +48,35 @@ class CardsList extends Component {
         })
     }
 
-    RenderCards(items) {
-        items.map(item => {
-            return <Card style={{ width: '18rem' }}><Card.Img variant="top" src={item.imageUrl} /><Card.Title>{item.name}</Card.Title></Card>
-        })
+    //Renderiza cada um dos cards que será exibido na tela com base nos dados obtidos
+    RenderCards(item) {
+        return (
+            <Col lg={3} md={4} sm={6} style={{ marginBottom: "38px" }}>
+                <Card style={{ backgroundColor: "#FFFFFF" }}>
+                    <Container>
+                        <Image src={item.imageUrl} roundedCircle style={{ width: "95px", height: "95px", backgroundColor: "#F6F4F6", marginTop: "28px" }}></Image>
+                    </Container>
+                    <Card.Body>
+                        <Card.Title>{item.name}</Card.Title>
+                        <hr
+                            style={{
+                                background: '#F0EFF0',
+                                color: '#F0EFF0',
+                                borderColor: '#F0EFF0',
+                                height: '3px',
+                            }}
+                        />
+                        <Card.Text>
+                            Lorem ipsum dolor sit amet consectetur.
+                        </Card.Text>
+                        <Row>
+                            <Col><Button variant="danger">Excluir</Button></Col>
+                            <Col><Button variant="light">Editar</Button></Col>
+                        </Row>
+                    </Card.Body>
+                </Card>
+            </Col>
+        )
     }
 
     componentDidMount() {
@@ -58,34 +84,62 @@ class CardsList extends Component {
     }
 
     render() {
-        const AllItems = this.state.AllPokeData
+        const AllItems = this.state.AllPokeData;
+        const SearchValue = this.state.SearchValue;
 
         return (
-            <div>
-                {/* {this.RenderCards(AllItems)} */}
-                <Row>
-                    {AllItems.length ? AllItems.map(item => {
-                        return <Col>
-                            <Card style={{ width: '18rem' }}>
-                                <Card.Img variant="top" src={item.imageUrl} />
-                                <Card.Title>{item.name}</Card.Title>
-                            </Card>
-                        </Col>
-                    }) : null}
-                </Row>
-                {/* <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src={item.imageUrl} />
-                    <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the
-                            bulk of the card's content.
-                        </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
-                    </Card.Body>
-                </Card> */}
-                {/* {Items.length ? Items.map(item => { return <div>{item.name}</div> }) : null} */}
-            </div>
+            <>
+                <Container fluid id="search-background">
+                    <Container id="search-container">
+                        <Row>
+                            <Col md={12}>
+                                <InputGroup id="search-bar">
+                                    <Form.Control
+                                        size="lg"
+                                        type="text"
+                                        placeholder="Digite aqui sua busca..."
+                                        aria-label="Digite aqui sua busca..."
+                                        style={{ paddingLeft: "27px" }}
+                                        onChange={(event) => {
+                                            let searchValue = event.target.value;
+                                            this.setState({ SearchValue: searchValue })
+                                        }}
+                                    >
+                                    </Form.Control>
+                                    <InputGroup.Text id="search-icon"></InputGroup.Text>
+                                    {/* <Button variant="light" id="button-addon">
+                                    </Button> */}
+                                </InputGroup>
+                            </Col>
+                        </Row>
+                    </Container>
+                </Container>
+
+                <Container fluid style={{ paddingTop: "10px", backgroundColor: "#F6F4F6" }}>
+                    <Container id="result-container">
+                        <Row id="results-title-row">
+                            <Col md={6} xs={6}>
+                                <h1 id="results-title">Resultado de busca</h1>
+                            </Col>
+                            <Col md={6} xs={6}>
+                                <Button id="button-new-card">Novo card</Button>
+                            </Col>
+                        </Row>
+                    </Container>
+
+                    <Container style={{ marginTop: "36px" }}>
+                        <Row className="text-center">
+                            {AllItems.length ? AllItems.filter(val => {
+                                if (SearchValue == "" || SearchValue == null) {
+                                    return val;
+                                } else if (val.name.toLowerCase().includes(SearchValue.toLowerCase())) {
+                                    return val;
+                                }
+                            }).map(item => { return this.RenderCards(item) }) : null}
+                        </Row>
+                    </Container>
+                </Container>
+            </>
         )
     }
 }
